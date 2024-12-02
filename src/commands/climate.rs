bd4f0065c70077519e7b03952ddd03f6aeb93051
Johannes Riedmann <johannes.riedmann.vlbg@gmail.com>
2024-12-02 01:23:55 +0100

    Resolved issue of bot responding to its own ephemeral message

--- src/commands/climate.rs
+++ src/commands/climate.rs
@@ -3,13 +3,13 @@ use poise::{serenity_prelude as serenity, CreateReply};
 use sources::geocoding;
 use sources::geocoding::Place;
 use sources::climate_forecast as forecast;
-use poise::serenity_prelude::CreateSelectMenuKind;
+use poise::serenity_prelude::{CreateSelectMenuKind};
 use serenity::CreateSelectMenuOption as MenuOption;
 
 /// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
+#[poise::command(slash_command, prefix_command, hide_in_help)]
 pub async fn age(ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
+                 #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
 
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
@@ -21,76 +21,98 @@ pub async fn age(ctx: Context<'_>,
 
 #[poise::command(slash_command, prefix_command, track_edits, aliases("temp"))]
 pub async fn temperature(ctx: Context<'_>,
-    #[description = "Name of a place"] place: String
+                         #[description = "Name of a place"] place: String,
 ) -> Result<(), Error> {
-
+    // look up the requested place
     let geo_result = geocoding::query_place(&place).await;
 
-    let response: String = match geo_result {
-        Ok(places) => {
-            if places.is_empty() {
-                format!("Could not find a matching place for `{}`", &place)
-            } else {
-                match select_place(ctx, &places, &place).await {
-                    Some(place) => {
-                        let data = forecast::get_current_temperature(place).await?;
-                        let msg = format!("The current temperature in **{}** is **`{}°C`** _(last updated: <t:{}:R>)_",
-                            place.name, data.temperature_2m, data.epoch);
-                        msg
-                    },
-                    None => "Place selection was cancelled".to_string(),
-                }
-            }
-        },
-        Err(e) => return Err(Box::new(e))
+    // unwrap the geocoding response
+    let places = match geo_result {
+        Ok(place_list) => place_list,
+        Err(e) => return Err(e.into()) // Err(Box::new(e))
     };
 
-    ctx.reply(response).await?;
+    if places.is_empty() {
+        ctx.reply(format!("Could not find a matching place for `{}`", &place)).await?;
+    } else {
+        // select a place from the list
+        match select_place(ctx, &places, &place).await {
+            Selection::Unique(place) => {
+                let response = get_current_temperature(place).await?;
+                ctx.reply(response).await?;
+            },
+            Selection::OneOfMany(place) => {
+                let response = get_current_temperature(place).await?;
+                ctx.channel_id().say(ctx.http(), response).await?; // maybe add "(invoked by @author)"?
+            },
+            Selection::Aborted => {
+                ctx.channel_id().say(ctx.http(), "Place selection was cancelled".to_string()).await?;
+            },
+        }
+    }
+
     Ok(())
 }
 
+pub enum Selection<T> {
+    Aborted,
+    Unique(T),
+    OneOfMany(T),
+}
+
+async fn get_current_temperature(place: &Place) -> Result<String, Error> {
+    let data = forecast::get_current_temperature(place).await?;
+    let msg = format!("The current temperature in **{}** is **`{}°C`** _(last updated: <t:{}:R>)_",
+                      place.name, data.temperature_2m, data.epoch);
+    Ok(msg)
+}
 
 // If first element matches the search term exactly and the second element does not, take the first one. Else, show the full list to pick from.
-async fn select_place<'a>(ctx: Context<'_>, places: &'a [Place], search_term: &str) -> Option<&'a Place> {
+async fn select_place<'a>(ctx: Context<'_>, places: &'a [Place], search_term: &str) -> Selection<&'a Place> {
     if places.is_empty() {
-        return None;
+        return Selection::Aborted;
     }
 
-    // vector has only one element or the first element matches exactly and the second already deviates from the search term
-    if places.len() == 1 || places.get(1)?.name == search_term && places.get(2)?.name != search_term {
-        return places.first();
+    // vector has only one element or only one that matches the search term exactly
+    let exact_matches: Vec<&Place> = places.iter().filter(|&item| item.name == search_term).collect();
+    if exact_matches.len() == 1 {
+        return Selection::Unique(exact_matches.first().unwrap());
     }
 
     request_user_selection(ctx, places).await
 }
 
-async fn request_user_selection<'a>(ctx: Context<'_>, places: &'a [Place]) -> Option<&'a Place> {
+async fn request_user_selection<'a>(ctx: Context<'_>, places: &'a [Place]) -> Selection<&'a Place> {
     const INTERACTION_ID: &str = "place_selection";
 
     let options: Vec<MenuOption> = places.iter().enumerate()
         .map(|(idx, p)| MenuOption::new(p.to_string(), idx.to_string()))
         .collect();
 
-    let components = vec![
-        serenity::CreateActionRow::SelectMenu(
-            serenity::CreateSelectMenu::new(
-                INTERACTION_ID,
-                CreateSelectMenuKind::String { options })
-                .placeholder("Select place")
-        ),
-    ];
+    // create select place prompt with the selection menu
+    let place_selection = {
+        let components = vec![
+            serenity::CreateActionRow::SelectMenu(
+                serenity::CreateSelectMenu::new(
+                    INTERACTION_ID,
+                    CreateSelectMenuKind::String { options })
+                    .placeholder("Select place")
+            ),
+        ];
 
-    // send the question with the selection menu
-    let place_selection = CreateReply::default()
-        .content("Which one of these is the place you are looking for?")
-        .components(components)
-        .ephemeral(true);
+        CreateReply::default()
+            .content("Which one of these is the place you are looking for?")
+            .components(components)
+            .ephemeral(true)
+            .reply(true)
+    };
+
     if (ctx.send(place_selection).await).is_err() {
-        return None;
+        return Selection::Aborted;
     }
 
     // react on the first interaction on the selection menu (with timeout)
-    if let Some(interaction) = serenity::ComponentInteractionCollector::new(ctx.serenity_context())
+    if let Some(interaction) = serenity::ComponentInteractionCollector::new(ctx)
         .author_id(ctx.author().id)
         .channel_id(ctx.channel_id())
         .timeout(std::time::Duration::from_secs(120))
@@ -104,10 +126,14 @@ async fn request_user_selection<'a>(ctx: Context<'_>, places: &'a [Place]) -> Op
 
         // acknowledge the interaction
         let _ = interaction.create_response(ctx, serenity::CreateInteractionResponse::Acknowledge).await;
+        interaction.delete_response(ctx.http()).await.ok();
+
         if let Ok(index) = selected_value.parse::<usize>() {
-            return places.get(index);
-        }
+            if let Some(place) = places.get(index) {
+                return Selection::OneOfMany(place);
+            }
+        };
     }
 
-    None
+    Selection::Aborted
 }
