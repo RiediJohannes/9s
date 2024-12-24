1cc74ddb88be830a50ec919f87022c4c669537bf
Johannes Riedmann <johannes.riedmann.vlbg@gmail.com>
2024-12-25 00:40:38 +0100

    Ensured reflexivity and transitivity of coordinate equals

--- src/sources/common.rs
+++ src/sources/common.rs
@@ -41,36 +41,48 @@ impl Coordinates {
 }
 impl PartialEq for Coordinates {
     fn eq(&self, other: &Self) -> bool {
-        // this is fine because we never do math with coordinates, we just use parsed string coordinates
-        self.latitude == other.latitude && self.longitude == other.longitude
+        self.latitude.thousandths() == other.latitude.thousandths() && 
+            self.longitude.thousandths() == other.longitude.thousandths()
     }
 }
 impl Eq for Coordinates {} // marker interface to guarantee that PartialEq implementation is reflexive
 impl Hash for Coordinates {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.latitude.to_bits().hash(state);
-        self.longitude.to_bits().hash(state);
+        self.latitude.thousandths().hash(state);
+        self.longitude.thousandths().hash(state);
+    }
+}
+
+trait Thousandth {
+    fn thousandths(&self) -> i32;
+}
+impl Thousandth for f64 {
+    fn thousandths(&self) -> i32 {
+        (self * 1000.0).round() as i32
     }
 }
 
 
-pub async fn query_api<O, S, F>(client: &reqwest::Client, url: &str, params: impl IntoIterator<Item = (&str, String)>)
-                                -> Result<O, ApiError>
+pub async fn query_api<TOutput, TSuccess, TFailure>(
+    client: &reqwest::Client,
+    url: &str,
+    params: impl IntoIterator<Item = (&str, String)>
+) -> Result<TOutput, ApiError>
 where
-    S: for<'de> Deserialize<'de> + Into<O>,
-    F: for<'de> Deserialize<'de> + Into<ApiError>,
+    TSuccess: for<'de> Deserialize<'de> + Into<TOutput>,
+    TFailure: for<'de> Deserialize<'de> + Into<ApiError>,
 {
     let url = reqwest::Url::parse_with_params(url, params)?;
 
     let response = client.get(url).send().await?;
     let payload = response.text().await?;
 
-    // try to parse the response body as the given success type S
-    match serde_json::from_str::<S>(&payload) {
+    // try to parse the response body as the given success type TSuccess
+    match serde_json::from_str::<TSuccess>(&payload) {
         Ok(current_temp_result) => Ok(current_temp_result.into()),
         Err(e) => {
-            // If it fails, attempt to parse the body as the given failure type F
-            match serde_json::from_str::<F>(&payload) {
+            // If it fails, attempt to parse the body as the given failure type TFailure
+            match serde_json::from_str::<TFailure>(&payload) {
                 Ok(api_error) => Err(api_error.into()),
                 Err(_) => Err(ApiError::Parsing(e)), // Return the error if both parsing attempts fail
             }
