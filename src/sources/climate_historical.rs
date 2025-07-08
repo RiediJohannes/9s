e26f6c781f3e304f39aaa46e46b9a9014cdba478
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-07-08 02:49:11 +0200

    Reuse get_temperature_series function from get_past_temperature

--- src/sources/climate_historical.rs
+++ src/sources/climate_historical.rs
@@ -1,13 +1,10 @@
 use crate::geo_time::Tz;
 use crate::sources::common;
 use crate::sources::common::{ApiError, ClimateApiError, Coordinates, SingleTemperature};
-use crate::sources::nominatim::Place;
 use chrono::DateTime;
-use poise::serenity_prelude::Timestamp;
 use serde::Deserialize;
 
 const BASE_URL: &str = "https://archive-api.open-meteo.com/v1/archive";
-// const CACHE_TTL_SECONDS: u64 = 120;
 
 #[derive(Deserialize, Debug)]
 struct HistoricalTemperature {
@@ -54,48 +51,33 @@ impl From<HistoricalTemperature> for Vec<TemperatureDataPoint> {
 }
 
 
-// TODO Think thoroughly about the caching strategy (add date (without time!) to the key, consider non-timed cache
-// #[cached(
-//     ty = "TimedCache<Coordinates, SingleTemperature>",
-//     create = "{ TimedCache::with_lifespan(CACHE_TTL_SECONDS) }",
-//     convert = r#"{ point.clone() }"#,
-//     result = true
-// )]
 pub async fn get_past_temperature(client: &reqwest::Client, point: &Coordinates, datetime: &DateTime<Tz>)
                                   -> Result<SingleTemperature, ApiError>
 {
-    let requested_date_iso = datetime.format("%Y-%m-%d").to_string();
-    let naive_date = datetime.date_naive().to_string();
+    let temperature_series = get_temperature_series(client, point, datetime, datetime).await?;
 
-    println!("Formatted datetime: {}", requested_date_iso);
-    println!("Naive date: {}", naive_date);
+    // TODO Grab the correct temperature point from the series according to the time of the time stamp
+    //      Note, the series only has hourly data. Also, it might make sense to change the Vec to a BTree or something similar
+    Ok(temperature_series.first().unwrap().clone().into())
+}
+
+pub async fn get_temperature_series(client: &reqwest::Client, point: &Coordinates,
+                                    start_date: &DateTime<Tz>, end_date: &DateTime<Tz>)
+    -> Result<Vec<TemperatureDataPoint>, ApiError>
+{
+    if start_date > end_date {
+        return Err(ApiError::BadRequest {
+            reason: "Start date must be less than or equal to end date".to_string(),
+        })
+    }
 
     let params = [
         ("latitude", point.latitude.to_string()),
         ("longitude", point.longitude.to_string()),
         ("hourly", "temperature_2m".to_string()),
-        ("start_date", requested_date_iso.clone()),
-        ("end_date", requested_date_iso),
-        ("timezone", "Europe/Berlin".to_string()),
-        ("timeformat", "unixtime".to_string()),
-    ];
-
-    let temperature_series = common::query_api::<Vec<TemperatureDataPoint>, HistoricalTemperature, ClimateApiError>
-        (client, BASE_URL, params).await?;
-
-    Ok(temperature_series.first().unwrap().clone().into())
-}
-
-pub async fn get_temperature_series(client: &reqwest::Client, place: &Place,
-                                    start_date: Timestamp, end_date: Timestamp)
-    -> Result<Vec<TemperatureDataPoint>, ApiError>
-{
-    let params = [
-        ("latitude", place.lat.to_string()),
-        ("longitude", place.lon.to_string()),
-        ("hourly", "temperature_2m".to_string()),
         ("start_date", start_date.date_naive().to_string()),
         ("end_date", end_date.date_naive().to_string()),
+        ("timezone", start_date.timezone().name().to_string()), // W.l.o.g we take the timezone of the start date
         ("timeformat", "unixtime".to_string()),
     ];
 
